1b3ff34874d4201cf18f3f3a715de31661c05532
Полина <polina_kazhina@mail.ru>
2023-01-05 13:20:42 +0300

    init: make reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let numberToString = String(Math.abs(n));
+    let finishNumber = numberToString.split('').reverse().join(''); 
+    return Number(finishNumber); 
 }
